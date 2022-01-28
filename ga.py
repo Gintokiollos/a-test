ac9faab5c1f67f901d4afba508c638692c152e5c
Gintokiollos <39335110+Gintokiollos@users.noreply.github.com>
2022-01-28 09:59:03 +0800

    ga

--- ga.py
+++ ga.py
@@ -1,7 +1,7 @@
 
 from urllib import request
 import re,requests
-
+a = []
 url = 'http://renxueyanjiu.com/index.php?m=content&c=index&a=show&catid=122&id=2951'
 html = requests.get(url)
 html.text
@@ -11,6 +11,11 @@ if html.status_code ==200:
     info3=(html.text)
     pattern3=re.compile(r'(?<=、)\w+')
     f3=pattern3.findall(info3)
+    gu=len(f3)
+    for i in range(gu):
+        if  len(f3[i]) != 4 or f3[i] == '人学研究' :
+            a.append(i)
+    print(a)
     print(f3,sep="\n")
 else:
     print('連線不成功')
