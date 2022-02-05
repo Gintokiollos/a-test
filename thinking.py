e0a5692da571d97ed814a67502a1a3fea1421165
Gintokiollos <39335110+Gintokiollos@users.noreply.github.com>
2022-02-05 21:37:55 +0800

    a

--- thinking.py
+++ thinking.py
@@ -1,21 +1,14 @@
-import sys
+from __future__ import unicode_literals
+import youtube_dl ,glob
 from moviepy.editor import *
-import you_get
-import os
-import glob
-def download(url, path):
-    sys.argv = ['you-get','-o', path ,'-l', url]
-    you_get.main()
-if __name__ == '__main__':
-     # 视频网站的地址
-    url = input("你需要下載的內容:")
-    # 视频输出的位置
-    path = input("下載位置:")
-    download(url, path)
-videoclip = VideoFileClip(path) #
+path='E:\a-test'
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/playlist?list=PLiFNg5fXiX32p4RMMDCUpjVEwXOnqyTj_'])
+videoclip = VideoFileClip(path)
 audioclip = videoclip.audio
 audioclip.write_audiofile(path)
 audioclip.close()
 videoclip.close()
-for infile in glob.glob(os.path.join(path,'*.srt')): #找出垃圾並刪除
+for infile in glob.glob(os.path.join(path,'*.mp4')): #找出垃圾並刪除
     os.remove(infile)
